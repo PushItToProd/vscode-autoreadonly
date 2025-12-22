2f588ec8a8b515473c6c8924a8bc63a0152ebd66
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-12-22 10:22:48 -0800

    rudimentary first pass - set readonly if file path contains "noedit"

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,43 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Keep track of the active editor.
+  let activeEditor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-automatic-readonly" is now active!');
+  function checkIfActiveEditorShouldBeReadOnly() {
+    if (!activeEditor) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-automatic-readonly.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-automatic-readonly!');
-	});
+    const fileUri = activeEditor.document.uri;
+    console.log("active editor URI:", fileUri);
 
-	context.subscriptions.push(disposable);
+    const filePath = fileUri.fsPath;
+
+    if (shouldBeReadOnly(filePath)) {
+      console.log("setting active editor to read only");
+      // TODO: keep track of seen editors and don't mark them read only every
+      // time. That way, we can make an editor writable if we want.
+      vscode.window.showInformationMessage("Making active editor read only");
+      setReadOnly();
+    }
+  };
+
+  function shouldBeReadOnly(filePath: string) {
+    // TODO: get patterns to match from settings
+    return filePath.indexOf("noedit") !== -1;
+  }
+
+  function setReadOnly() {
+    vscode.commands.executeCommand("workbench.action.files.setActiveEditorReadonlyInSession");
+  }
+
+  // TODO: this isn't fired if the editor is active when the window is first
+  // opened.
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    activeEditor = editor;
+    if (editor) {
+      checkIfActiveEditorShouldBeReadOnly();
+    }
+  }, null, context.subscriptions);
 }
 
 // This method is called when your extension is deactivated
