6cc9cf0e9d87b1821f406013967ef2f8f87d7ad7
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-12-22 11:57:44 -0800

    delete initial comments and a stray newline

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
   function getReadOnlyPatterns(uri: vscode.Uri): object | null {
@@ -48,7 +44,6 @@ export function activate(context: vscode.ExtensionContext) {
     }
   };
 
-
   function setReadOnly() {
     vscode.commands.executeCommand("workbench.action.files.setActiveEditorReadonlyInSession");
   }
@@ -67,5 +62,4 @@ export function activate(context: vscode.ExtensionContext) {
   checkIfActiveEditorShouldBeReadOnly();
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
